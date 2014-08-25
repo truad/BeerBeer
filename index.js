30da3beba5168f3f845cb7016f117886938d03b0
Truadpon <truad.inventio@gmail.com>
2014-08-25 23:25:57 +0700

    Add index.js as main start script

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Agile on 8/25/14.
+ * Description : Start up script
+ */
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
